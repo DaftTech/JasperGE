54c5c0a2d2539baff2aa06be484ec086504580b7
LolHens <pierrekisters@gmail.com>
2016-01-16 18:39:48 +0100

    added VBO and Pointer

---
+++ src/main/scala/com/dafttech/jasper/gl/Pointer.scala
@@ -0,0 +1,52 @@
+package com.dafttech.jasper.gl
+
+import org.lwjgl.opengl.GL11._
+
+abstract class Pointer {
+  def setup(stride: Int, offset: Int): Unit
+
+  def stride(pointers: Seq[Pointer]): Int
+}
+
+object Pointer {
+  def setup(pointers: Seq[Pointer]) = {
+    val stride = pointers.foldLeft(0)((last, e) => last + e.stride(pointers))
+
+    pointers.foldLeft(0) { (last, e) =>
+      e.setup(stride, last)
+      last + e.stride(pointers)
+    }
+
+    stride
+  }
+
+  class VertexPointer(size: Int) extends Pointer {
+    override def setup(stride: Int, offset: Int): Unit = glVertexPointer(size, GL_FLOAT, stride, offset)
+
+    override def stride(pointers: Seq[Pointer]): Int = 4 * size
+  }
+
+  class ColorPointer(size: Int) extends Pointer {
+    override def setup(stride: Int, offset: Int): Unit = glColorPointer(size, GL_FLOAT, stride, offset)
+
+    override def stride(pointers: Seq[Pointer]): Int = 4 * size
+  }
+
+  class TexCoordPointer(size: Int) extends Pointer {
+    override def setup(stride: Int, offset: Int): Unit = glTexCoordPointer(size, GL_FLOAT, stride, offset)
+
+    override def stride(pointers: Seq[Pointer]): Int = 4 * size
+  }
+
+  class NormalPointer extends Pointer {
+    override def setup(stride: Int, offset: Int): Unit = glNormalPointer(GL_FLOAT, stride, offset)
+
+    override def stride(pointers: Seq[Pointer]): Int = pointers.foldLeft[Option[Int]](None) { (last, e) =>
+      e match {
+        case vertexPointer: VertexPointer => Some(vertexPointer.stride(pointers))
+        case _ => last
+      }
+    }.getOrElse(0)
+  }
+
+}
---
+++ src/main/scala/com/dafttech/jasper/gl/VBO.scala
@@ -0,0 +1,62 @@
+package com.dafttech.jasper.gl
+
+import java.nio.ByteBuffer
+
+import org.lwjgl.opengl.GL11._
+import org.lwjgl.opengl.GL15._
+
+class VBO(size: Int, pointers: Seq[Pointer]) {
+  private var timesBound = 0
+
+  val vboId = gen
+  val stride = Pointer.setup(pointers)
+  allocate(size)
+
+  private def gen: Int = glGenBuffers()
+
+  private def allocate(size: Int) = {
+    bind
+    glBufferData(GL_ARRAY_BUFFER, size, null, GL_DYNAMIC_DRAW)
+    unbind
+  }
+
+  def put(buffer: ByteBuffer, offset: Long, length: Int): Unit = {
+    bind
+    glBufferSubData(GL_ARRAY_BUFFER, offset, length, buffer)
+    unbind
+  }
+
+  def bind: Unit = {
+    timesBound += 1
+    if (timesBound == 1) glBindBuffer(GL_ARRAY_BUFFER, vboId)
+  }
+
+  def unbind: Unit = {
+    timesBound -= 1
+    if (timesBound == 0) glBindBuffer(GL_ARRAY_BUFFER, 0)
+  }
+
+  def render: Unit = render(0, size)
+
+  def render(offset: Int, length: Int): Unit = {
+    bind
+    glDrawArrays(GL_TRIANGLES, offset, length / stride)
+    unbind
+  }
+
+  def close = glDeleteBuffers(vboId)
+}
+
+object VBO {
+  private var enabled = false
+
+  def setup() = {
+    if (!enabled) {
+      glEnableClientState(GL_VERTEX_ARRAY)
+      glEnableClientState(GL_NORMAL_ARRAY)
+      glEnableClientState(GL_COLOR_ARRAY)
+      glEnableClientState(GL_TEXTURE_COORD_ARRAY)
+      enabled = true
+    }
+  }
+}
